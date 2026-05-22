b6a2e3a22a23b303bbeaa88ed453b443da329d45
Brigitta Lucia <blohutabarat@gmail.com>
2026-05-22 19:36:24 +0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,25 @@
+const hre = require("hardhat");
 
+async function main() {
+
+    const Factory = await hre.ethers.getContractFactory(
+        "CourseRewardSystem"
+    );
+
+    const contract = await Factory.deploy(
+        hre.ethers.parseEther("1"),
+        7
+    );
+
+    await contract.waitForDeployment();
+
+    console.log(
+        "Contract deployed to:",
+        await contract.getAddress()
+    );
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
