686ee7a48af1ad3f489ded8c081e5f01db9e2747
Brigitta Lucia <blohutabarat@gmail.com>
2026-05-22 19:36:57 +0700

    Update interact.js

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,1 +1,24 @@
+const hre = require("hardhat");
 
+async function main() {
+
+    const contractAddress = "PASTE_CONTRACT_ADDRESS";
+
+    const contract = await hre.ethers.getContractAt(
+        "CourseRewardSystem",
+        contractAddress
+    );
+
+    const tx = await contract.setRewardAmount(
+        hre.ethers.parseEther("2")
+    );
+
+    await tx.wait();
+
+    console.log("Reward updated");
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
